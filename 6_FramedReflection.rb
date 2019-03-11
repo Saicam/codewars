7b94e0f3d5ace1314be40c96375301926a254604
Jose Macias <josemariamacias@gmail.com>
2019-03-11 15:55:46 +0000

    Finished Framed Reflection Cata

--- 6_FramedReflection.rb
+++ 6_FramedReflection.rb
@@ -1,9 +1,18 @@
 # https://www.codewars.com/kata/framed-reflection
 #!/usr/bin/env ruby
-
 def mirror(text)
   x = 0
-  text.split(" ").each do |word| x = word.reverse!.length }
+  aux = text.split(" ")
+  aux_txt = ""
+  aux.each do |word|
+     x = word.length if x < word.reverse!.length
   end
+  aux_txt = "*" * (x + 4) + "\n"
+  aux.each do |word|
+    aux_txt += "* " + word + " " * (x - word.length) + " *\n"
+  end
+   return aux_txt += "*" * (x + 4)
+
 end
-puts mirror("Hello World")
+
+puts mirror("Hello Worlds")
